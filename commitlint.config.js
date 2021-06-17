b59b78fbd99621f3e3a649be97fb29bb83e0caf2
giolf <giovanni.far@nemesigroup.ch>
2021-06-17 19:55:45 +0200

    test

---
+++ commitlint.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+    extends: ['@commitlint/config-conventional'],
+    rules: {
+        'references-empty': [2, 'never'],
+        'type-empty': [0],
+        'type-enum': [0]
+    },
+    parserPreset: {
+        parserOpts: {
+            headerCorrespondence: ['subject'],
+            headerPattern: /^[A-Z]{1,5}-[0-9]{1,4}: (\w+\s)*\w+$/,
+            issuePrefixes: ['GP-']
+        }
+    },
+}
