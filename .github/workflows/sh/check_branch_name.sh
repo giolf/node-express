20b12bad2b1958865cbfbc651f766b80dfa5dbc6
Giovanni Far <giovanni.far@nemesigroup.ch>
2021-07-04 18:32:28 +0200

    Update check_branch_name.sh

--- .github/workflows/sh/check_branch_name.sh
+++ .github/workflows/sh/check_branch_name.sh
@@ -1,12 +1,8 @@
-BASE_REF=$1
-HEAD_REF=$2
-
-
-if [[ $BASE_REF == "develop" ]] && [[ ! $HEAD_REF =~ ^feature\/GP-[0-9]+$ ]]; then
+if [[ ${{ github.base_ref }} == "develop" ]] && [[ ! ${{ github.head_ref }} =~ ^feature\/GP-[0-9]+$ ]]; then
   echo "::error::Error message for dev"
   exit 1
 fi
-if [[ $BASE_REF == "master" ]] && [[ ! $HEAD_REF =~ ^(release|hotfix)\/GP-[0-9]+$ ]]; then
+if [[ ${{ github.base_ref }} == "master" ]] && [[ ! ${{ github.head_ref }} =~ ^(release|hotfix)\/GP-[0-9]+$ ]]; then
   echo "::error::Error message for master"
   exit 1
 fi
