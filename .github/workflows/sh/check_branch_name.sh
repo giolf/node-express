13ac59a4d44dabebed522a38ee70cd1eafdc1542
Giovanni Far <giovanni.far@nemesigroup.ch>
2021-07-08 17:56:13 +0200

    Update check_branch_name.sh

--- .github/workflows/sh/check_branch_name.sh
+++ .github/workflows/sh/check_branch_name.sh
@@ -2,10 +2,10 @@ BASE_REF=$1
 HEAD_REF=$2
 
 if [[ $BASE_REF == "develop" ]] && [[ ! $HEAD_REF =~ ^feature\/GP-[0-9]+$ ]]; then
-  echo "::error::Error message for dev"
+  echo "::error::Only 'feature' named branches can be merged into develop"
   exit 1
 fi
 if [[ $BASE_REF == "master" ]] && [[ ! $HEAD_REF =~ ^(release|hotfix)\/GP-[0-9]+$ ]]; then
-  echo "::error::Error message for master"
+  echo "::error::Only 'release' or 'hotfix named branches can be merged into develop"
   exit 1
 fi
